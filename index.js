66063c8e8def37661b452dd52b319ed35b76b358
Tim Eitel <timeitel@outlook.com>
2020-12-18 16:00:33 +0800

    feat: export to json file

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const fs = require('fs');
 const csvFilePath = './data.csv';
 const csv = require('csvtojson');
-csv()
-  .fromFile(csvFilePath)
-  .then((jsonObj) => {
-    const jsonFile = JSON.stringify(jsonObj);
 
-    fs.writeFile('data.json', jsonFile, (err) => {
-      if (err) {
-        throw err;
-      }
-      console.log('JSON data exported.');
-    });
+convertToJSON();
+
+async function convertToJSON() {
+  const jsonArray = await csv().fromFile(csvFilePath);
+
+  // parse and reformat
+  //   jsonFormatted = Object.entries(jsonArray).map(([k, v]) => {
+  //     return {};
+  //   });
+
+  fs.writeFile('data.json', JSON.stringify(jsonArray), (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('JSON data exported.');
   });
+}
