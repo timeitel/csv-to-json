46e632e0ed5b7fd1f03f8a78f9b9f7c9d20356f3
Tim Eitel <timeitel@outlook.com>
2021-01-13 12:24:22 +0800

    feat: loop over csv files, convert and post to elastic

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import { postToElasticSearch } from './src/util.js';
 import { convertToJSON } from './src/converter.js';
+import fs from 'fs';
 
-convertAndPostToElasticSearch('./assets/import-data.csv');
-
-async function convertAndPostToElasticSearch(pathToCSV) {
+async function convertCsvToJsonAndPostToElastic(pathToCSV) {
   const questions = await convertToJSON(pathToCSV);
   postToElasticSearch(questions);
 
   console.log('Finished converting and posting to Elasticsearch');
 }
+
+function startFromDirectory(folder) {
+  try {
+    const csvFiles = fs.readdirSync(folder);
+
+    csvFiles.forEach((file) => {
+      convertCsvToJsonAndPostToElastic(`./assets/questions/${file}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+startFromDirectory('./assets/questions');
