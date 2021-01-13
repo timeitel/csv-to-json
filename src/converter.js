4f217de556258a076cc2eecbbdca1bdf6c925d34
Tim Eitel <timeitel@outlook.com>
2021-01-13 13:02:57 +0800

    feat: add category field to records

--- src/converter.js
+++ src/converter.js
@@ -5,7 +5,8 @@ const { csv } = pkg;
 async function convertToJSON(csvFilePath) {
   let jsonQuestion = '';
   const questions = await csv().fromFile(csvFilePath);
-
+  const file = csvFilePath.split('/').pop();
+  const fileName = file.substring(0, file.indexOf('.'));
   const labels = mostCommonWords(questions);
 
   // parse and reformat to be bulk imported into Elasticsearch
@@ -15,7 +16,7 @@ async function convertToJSON(csvFilePath) {
     }
 
     jsonQuestion += '{ "index": {} }\n';
-    jsonQuestion += `{ "QUESTION": "${q.QUESTION}", "CORRECT": "1", "ANSWER_1": "${q.ANSWER}", "ANSWER_2": "${q.WRONG_1}", "ANSWER_3": "${q.WRONG_2}", "ANSWER_4": "${q.WRONG_3}", "LABELS": "${labels}" }\n`;
+    jsonQuestion += `{ "QUESTION": "${q.QUESTION}", "CORRECT": "1", "ANSWER_1": "${q.ANSWER}", "ANSWER_2": "${q.WRONG_1}", "ANSWER_3": "${q.WRONG_2}", "ANSWER_4": "${q.WRONG_3}", "CATEGORY": "${fileName}", "LABELS": "${labels}" }\n`;
   }
   console.log('Finished formatting to Elasticsearch json');
 
